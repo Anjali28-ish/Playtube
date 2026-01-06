86e5c858b3d9749de1a4666ae2f0391d1fddf886
Anjali28-ish <anjalikumarimishra28@gmail.com>
2026-01-07 00:40:34 +0530

    Refactor GetCurrentUser to use api instance

--- frontend/src/customHooks/GetCurrentUser.jsx
+++ frontend/src/customHooks/GetCurrentUser.jsx
@@ -1,31 +1,33 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setUserData } from '../redux/userSlice'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import api from "../api/api"; // 👈 api instance
+import { setUserData } from "../redux/userSlice";
 
 const GetCurrentUser = () => {
-  const dispatch = useDispatch()
-  const{channelData} = useSelector(state=>state.user)
+  const dispatch = useDispatch();
+  const { channelData } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const result = await axios.get(
-          serverUrl + "/api/user/getuser",
-          { withCredentials: true }
-        )
-        dispatch(setUserData(result.data))
-        console.log(result.data)
+        const result = await api.get("/api/user/getuser");
+        // 👆 Authorization header auto attach hoga
+
+        dispatch(setUserData(result.data));
+        console.log("Current user:", result.data);
       } catch (error) {
-        console.log(error)
-        dispatch(setUserData(null))
+        console.error(
+          "GetCurrentUser error:",
+          error.response?.data || error.message
+        );
+        dispatch(setUserData(null));
       }
-    }
+    };
 
-    fetchUser()
-  }, [channelData])
+    fetchUser();
+  }, [dispatch, channelData]);
 
-}
+  return null;
+};
 
-export default GetCurrentUser
+export default GetCurrentUser;
